c4a2af8390c407ac6d3fc0924d1537212e97c8ac
Smith <dsmith11@c02y4398jgh5.mi.corp.rockfin.com>
2019-05-24 21:22:48 -0400

    WIP

--- soft-actor-critic/docker/includes/get-dqn.sh
+++ soft-actor-critic/docker/includes/get-dqn.sh
@@ -1,2 +1,4 @@
 cd /
 git clone https://github.com/DanielSmithMichigan/reinforcement-learning.git
+cd /reinforcement-learning
+git clone
--- soft-actor-critic/runTest.py
+++ soft-actor-critic/runTest.py
@@ -2,6 +2,7 @@ import MySQLdb
 import os
 from agent.Agent import Agent
 import numpy as np
+import nevergrad as ng
 import tensorflow as tf
 import gym
 db = MySQLdb.connect(host="dqn-db-instance.coib1qtynvtw.us-west-2.rds.amazonaws.com", user="dsmith682101", passwd=os.environ['MYSQL_PASS'], db="dqn_results")
@@ -9,58 +10,76 @@ db = MySQLdb.connect(host="dqn-db-instance.coib1qtynvtw.us-west-2.rds.amazonaws.
 experimentName = "soft-actor-critic-general"
 env = gym.make('LunarLander-v2')
 
-entropyCoefficient = 10 ** np.random.uniform(-5, 0)
-learningRate = 10 ** np.random.uniform(-5, -2)
-rewardScaling = 10 ** np.random.uniform(-4, 1)
-actionScaling = 10 ** np.random.uniform(-1, 1)
-weightRegularizationConstant = 10 ** np.random.uniform(-3,-1)
+entropyCoefficientArg = Gaussian(mean=-2, std=2)
+learningRateArg = Gaussian(mean=-3, std=2)
+rewardScalingArg = Gaussian(mean=-3, std=2)
+actionScalingArg = Gaussian(mean=-1, std=1)
+weightRegularizationConstantArg = Gaussian(mean=-2, std=1)
 
-agent = Agent(
-    name="agent_"+str(np.random.randint(low=1000000,high=9999999)),
-    policyNetworkSize=[256, 256],
-    qNetworkSize=[256, 256],
-    valueNetworkSize=[256, 256],
-    entropyCoefficient=entropyCoefficient,
-    valueNetworkLearningRate=learningRate,
-    policyNetworkLearningRate=learningRate,
-    qNetworkLearningRate=learningRate,
-    tau=0.005,
-    gamma=0.99,
-    maxMemoryLength=int(1e6),
-    priorityExponent=0,
-    batchSize=256,
-    maxGradientNorm=5,
-    maxEpisodes=1024,
-    trainSteps=1024,
-    minStepsBeforeTraining=1024,
-    rewardScaling=rewardScaling,
-    actionScaling=actionScaling,
-    actionShift=0.0,
-    stepsPerUpdate=1,
-    render=False,
-    showGraphs=False,
-    weightRegularizationConstant=weightRegularizationConstant,
-    testSteps=1024,
-    maxMinutes=5
-)
+value = Gaussian(mean=-5000, std=2000)
 
-result = agent.execute()
-cur = db.cursor()
-cur.execute("insert into experiments (label, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, y) values ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}', '{11}')".format(
-        experimentName,
-        entropyCoefficient,
-        learningRate,
-        rewardScaling,
-        actionScaling,
-        weightRegularizationConstant,
-        0,
-        0,
-        0,
-        0,
-        0,
-        result
-    )
-)
-db.commit()
-cur.close()
-db.close()
+instrumentation = ng.Instrumentation(entropyCoefficientArg, learningRateArg, rewardScalingArg, actionScalingArg, weightRegularizationConstantArg, value=value)
+optimizer = ng.optimizers.registry["TBPSA"](instrumentation=instrumentation, budget=200)
+
+db.query("select x1, x2, x3, x4, x5, y from experiments where experimentName = '"+experimentName+"'")
+result = db.store_result()
+for i in range(1000):
+    row = result.fetch_row(maxrows=0, how=1)
+    print(row)
+# candidate = optimizer.create_candidate.from_call(*args, **kwargs)
+
+# entropyCoefficient = 10 ** np.random.uniform(-5, 0)
+# learningRate = 10 ** np.random.uniform(-5, -2)
+# rewardScaling = 10 ** np.random.uniform(-4, 1)
+# actionScaling = 10 ** np.random.uniform(-1, 1)
+# weightRegularizationConstant = 10 ** np.random.uniform(-3,-1)
+
+# agent = Agent(
+#     name="agent_"+str(np.random.randint(low=1000000,high=9999999)),
+#     policyNetworkSize=[64, 64],
+#     qNetworkSize=[64, 64],
+#     valueNetworkSize=[64, 64],
+#     entropyCoefficient=entropyCoefficient,
+#     valueNetworkLearningRate=learningRate,
+#     policyNetworkLearningRate=learningRate,
+#     qNetworkLearningRate=learningRate,
+#     tau=0.005,
+#     gamma=0.99,
+#     maxMemoryLength=int(1e6),
+#     priorityExponent=0,
+#     batchSize=256,
+#     maxGradientNorm=5,
+#     maxEpisodes=1024,
+#     trainSteps=1024,
+#     minStepsBeforeTraining=4096,
+#     rewardScaling=rewardScaling,
+#     actionScaling=actionScaling,
+#     actionShift=0.0,
+#     stepsPerUpdate=1,
+#     render=False,
+#     showGraphs=False,
+#     weightRegularizationConstant=weightRegularizationConstant,
+#     testSteps=1024,
+#     maxMinutes=5
+# )
+
+# result = agent.execute()
+# cur = db.cursor()
+# cur.execute("insert into experiments (label, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, y) values ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}', '{11}')".format(
+#         experimentName,
+#         entropyCoefficient,
+#         learningRate,
+#         rewardScaling,
+#         actionScaling,
+#         weightRegularizationConstant,
+#         0,
+#         0,
+#         0,
+#         0,
+#         0,
+#         result
+#     )
+# )
+# db.commit()
+# cur.close()
+# db.close()
