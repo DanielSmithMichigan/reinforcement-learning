5d6c1418ae4a044936ff29086a973af28c26aca6
DQI <dsmith@ltu.edu>
2019-05-25 08:41:48 -0400

    Fixed reference

--- soft-actor-critic/runTest.py
+++ soft-actor-critic/runTest.py
@@ -22,7 +22,7 @@ value = ng.instrumentation.variables.Gaussian(mean=-5000, std=2000)
 instrumentation = ng.Instrumentation(entropyCoefficientArg, learningRateArg, rewardScalingArg, actionScalingArg, weightRegularizationConstantArg, value=value)
 optimizer = ng.optimizers.registry["TBPSA"](instrumentation=instrumentation, budget=os.environ['BUDGET'])
 
-cur.execute("select x1, x2, x3, x4, x5, y from experiments where experimentName = '"+experimentName+"'")
+cur.execute("select x1, x2, x3, x4, x5, y from experiments where label = '"+experimentName+"'")
 result = cur.fetchall()
 for row in result:
     candidate = optimizer.create_candidate.from_call(float(row[0]), float(row[1]), float(row[2]), float(row[3]), float(row[4]))
